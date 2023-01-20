837cc3189ce993f6f9f09e603af9b0de3cc8752d
96hsjeong <96.hs.jeong@gmail.com>
2023-01-20 14:45:47 +0900

    [S08P12A809-45] Update: 백엔드 초기 세팅

---
+++ back/imfine/src/main/java/com/idle/imfine/ImfineApplication.java
@@ -0,0 +1,13 @@
+package com.idle.imfine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImfineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImfineApplication.class, args);
+	}
+
+}
