1139b83d4c9fdcff1be584be9cf944cc9873744c
yoonjinlee <yoonjinifer@gmail.com>
2023-01-25 03:06:04 +0900

    [S08P12A809-121] Update: 네비게이션 바 styled-component 생성

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
-
+import NavBarBasic from './components/NavBar/NavBarBasic';
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <NavBarBasic />
+    </div>
+  );
 }
 
 export default App;
