c99525fbaed37b554c9c3a7c12daffe0cf6d2a34
김경림 <myway01212@naver.com>
2023-01-25 17:18:30 +0900

    [S08P12A809-127] Update: 모달 컴포넌트 기능 구현

--- front/src/components/Modal/Modal.js
+++ front/src/components/Modal/Modal.js
@@ -1,7 +1,9 @@
+import { useRef } from 'react';
+import useModalClickOutside from '../../hooks/useModalClickOutside';
 import {
   BoxModal,
   BoxInnerModal,
-  ModalClose,
+  // ModalClose,
   ModalContent,
   CloverImg,
   SpeechBubble,
@@ -11,13 +13,34 @@ import {
   BtnCircle,
 } from './style';
 
-function Modal({ text }) {
+function Modal({ type, setModalOpen }) {
+  /**
+   * 상위 컴포넌트에서 세팅해야할 것
+   * 액션 클릭했을때 Modal 컴포넌트 보여주기
+  const [modalOpen, setModalOpen] = useState(false);
+  <button onClick={() => setModalOpen(true)}>클릭해야할것</button>
+  {modalOpen && (
+    <Modal setModalOpen={setModalOpen} />
+    )}
+    */
+
+  const ref = useRef();
+  useModalClickOutside(ref, () => {
+    setModalOpen(false);
+  });
+
+  // api 세팅 완료되면 아래처럼 종류에 따라 다른 텍스트 적용 필요
+  let text;
+  if (type === '댓글신고') {
+    text = '해당 댓글을 신고하시겠습니까?';
+  }
+
   return (
     <BoxModal>
-      <BoxInnerModal>
-        <ModalClose>
+      <BoxInnerModal ref={ref}>
+        {/* <ModalClose ref={ref} onClick={() => setModalOpen(false)}>
           <img src="/assets/icons/close.png" alt="닫기" width="20px" />
-        </ModalClose>
+        </ModalClose> */}
         <ModalContent>
           <CloverImg />
           <SpeechBubble>{text}</SpeechBubble>
