eb3a7962e2aaf7b0982120b567e4b4ea89bb79ee
hani <hanikim2057@gmail.com>
2023-01-25 10:42:52 +0900

    [S08P12A809-131] Update: BoxNoShad 컴포넌트 수정

--- front/src/components/BoxNoShad/BoxNoShad.js
+++ front/src/components/BoxNoShad/BoxNoShad.js
@@ -1,14 +1,11 @@
 import styled from 'styled-components';
-// import React from 'react';
-
-// const BoxNoShad = () => {
-//   return <div>BoxNoShad</div>;
-// };
 
 export const BoxNoShad = styled.div`
-  background-color: ${(props) => props.color || '#F8FAF9'}
+  background-color: ${(props) => props.color || '#F8FAF9'};
   border-radius: ${(props) => props.radius || '25px'};
   border: none;
   padding: 24px;
   margin: 24px;
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
 `;
