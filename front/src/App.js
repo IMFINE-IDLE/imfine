175480c8f70686503597e462c27c636b853d053a
김경림 <myway01212@naver.com>
2023-01-26 17:55:14 +0900

    [S08P12A809-145] Update: Login 페이지 생성

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import DiaryCreate from './pages/DiaryCreate/DiaryCreate';
+import Login from './pages/Login/Login';
 import PaperCreate from './pages/PaperCreate/PaperCreate';
 import PaperFeed from './pages/PaperFeed/PaperFeed';
 
 function App() {
   return (
     <Routes>
-      <Route index element={<PaperFeed />} />
+      <Route index element={<Login />} />
+      <Route path="/home" element={<PaperFeed />} />
       <Route path="/diary">
         <Route path="/diary/create" element={<DiaryCreate />} />
       </Route>
---
+++ front/src/pages/Login/style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { BoxGrad } from '../../components/common/BoxGrad/BoxGrad';
+
+const BoxLogin = styled(BoxGrad)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export { BoxLogin };
