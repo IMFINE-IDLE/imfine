0d6eae323d20adf816ddac0b8e073f795b0d28cf
김경림 <myway01212@naver.com>
2023-01-26 14:23:20 +0900

    [S08P12A809-128] Update: DiaryCreate, PaperCreate 페이지 라우터 링크 추가

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
+import DiaryCreate from './pages/DiaryCreate/DiaryCreate';
+import PaperCreate from './pages/PaperCreate/PaperCreate';
 import PaperFeed from './pages/PaperFeed/PaperFeed';
 
 function App() {
   return (
     <Routes>
       <Route index element={<PaperFeed />} />
+      <Route path="/diary">
+        <Route path="/diary/create" element={<DiaryCreate />} />
+      </Route>
+      <Route path="/paper">
+        <Route path="/paper/create" element={<PaperCreate />} />
+      </Route>
     </Routes>
   );
 }
---
+++ front/src/pages/PaperCreate/PaperCreate.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function PaperCreate() {
+  return (
+    <div>
+      PaperCreate
+      <div>일기 작성 페이지</div>
+    </div>
+  );
+}
+
+export default PaperCreate;
