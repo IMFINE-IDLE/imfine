3481cf8c0d805f4e64afa39409a035510b155a73
김경림 <myway01212@naver.com>
2023-01-27 13:50:06 +0900

    [S08P12A809-146] Update: 회원가입 화면 라우터 링크 추가

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,29 @@
 import { Route, Routes } from 'react-router-dom';
+// import styled from 'styled-components';
 import DiaryCreate from './pages/DiaryCreate/DiaryCreate';
 import PaperCreate from './pages/PaperCreate/PaperCreate';
 import PaperFeed from './pages/PaperFeed/PaperFeed';
 import ProfilePage from './pages/ProfilePage/ProfilePage';
+import SignUp from './pages/SignUp/SignUp';
+
+// 뷰포트 사이즈 결정 필요
+// const Wrapper = styled.div`
+//   margin: 0 auto;
+//   width: 100%;
+//   max-width: 400px;
+//   height: 100vh;
+//   @media screen and (min-width: 400px) {
+//     background: none;
+//   }
+// `;
 
 function App() {
   return (
+    // <Wrapper>
     <Routes>
       <Route index element={<PaperFeed />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/home" element={<PaperFeed />} />
       <Route path="/diary">
         <Route path="/diary/create" element={<DiaryCreate />} />
       </Route>
@@ -20,6 +36,7 @@ function App() {
         <Route path="subscribe" /> */}
       </Route>
     </Routes>
+    // </Wrapper>
   );
 }
 
