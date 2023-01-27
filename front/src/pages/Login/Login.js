7e30f59d5345cbaefc2818862d13f847ae5e0e1f
김경림 <myway01212@naver.com>
2023-01-27 09:48:46 +0900

    [S08P12A809-145] Update: 스타일 중간 커밋

--- front/src/pages/Login/Login.js
+++ front/src/pages/Login/Login.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import { InputGray } from '../../components/common/InputGray/InputGray';
-import { BoxLogin } from './style';
+import { BoxLogin, BoxInnerLogin } from './style';
 
 function Login() {
   return (
     <BoxLogin radius={'0'} margin={'0'} height={'100vh'}>
       <img src="/assets/images/logo.png" alt="아임파인" width="280" />
-      <div>
-        <InputGray />
-      </div>
+      <BoxInnerLogin>
+        <form action="submit">
+          <div>
+            <label for="loginInput">로그인</label>
+            <InputGray
+              id="loginInput"
+              type="text"
+              required
+              autofocus
+              maxLength="12"
+            />
+          </div>
+          <div>
+            <label for="passwordInput">비밀번호</label>
+            <InputGray id="passwordInput" type="password" required />
+          </div>
+        </form>
+      </BoxInnerLogin>
     </BoxLogin>
   );
 }
