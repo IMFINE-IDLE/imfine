47fd0686381b548123f12cefc3e0117810158f98
김경림 <myway01212@naver.com>
2023-01-27 10:37:44 +0900

    [S08P12A809-145] Update: 로그인 스타일 중간 커밋

--- front/src/pages/Login/Login.js
+++ front/src/pages/Login/Login.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { InputGray } from '../../components/common/InputGray/InputGray';
-import { BoxLogin, BoxInnerLogin } from './style';
+import { BoxLogin, BoxInnerLogin, BoxInput, BtnLogin } from './style';
 
 function Login() {
   return (
     <BoxLogin radius={'0'} margin={'0'} height={'100vh'}>
       <img src="/assets/images/logo.png" alt="아임파인" width="280" />
       <BoxInnerLogin>
-        <form action="submit">
-          <div>
+        <form action="">
+          <BoxInput>
             <label for="loginInput">로그인</label>
             <InputGray
               id="loginInput"
@@ -16,12 +16,21 @@ function Login() {
               required
               autofocus
               maxLength="12"
+              padding={'1em'}
             />
-          </div>
-          <div>
+          </BoxInput>
+          <BoxInput>
             <label for="passwordInput">비밀번호</label>
-            <InputGray id="passwordInput" type="password" required />
-          </div>
+            <InputGray
+              id="passwordInput"
+              type="password"
+              required
+              padding={'1em'}
+            />
+          </BoxInput>
+          <BtnLogin margin={'0'} padding={'1em'} fontSize={'1em'} type="submit">
+            로그인하기
+          </BtnLogin>
         </form>
       </BoxInnerLogin>
     </BoxLogin>
--- front/src/pages/Login/style.js
+++ front/src/pages/Login/style.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { BoxGrad } from '../../components/common/BoxGrad/BoxGrad';
+import { Btn } from '../../components/common/Btn/Btn';
 
 const BoxLogin = styled(BoxGrad)`
   display: flex;
@@ -8,10 +9,24 @@ const BoxLogin = styled(BoxGrad)`
   flex-direction: column;
 `;
 
+const BoxLogo = styled.div`
+  height: 50vh;
+  max-width: 280px;
+  /* margin: 2em 0; */
+`;
+
 const BoxInnerLogin = styled.div`
   width: 100%;
   max-width: 280px;
   margin: 2em 0;
 `;
 
-export { BoxLogin, BoxInnerLogin };
+const BoxInput = styled.div`
+  margin: 1.2em 0;
+`;
+
+const BtnLogin = styled.button`
+  ${Btn}
+`;
+
+export { BoxLogin, BoxLogo, BoxInnerLogin, BoxInput, BtnLogin };
