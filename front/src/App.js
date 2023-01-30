79d5f8dc3ae0c9ebf632127566f4801686c0e120
2yoonjinny <2yoonjinny@naver.com>
2023-01-30 14:42:09 +0900

    [S08P12A809-167] Update: 대나무숲 화면 경로설정

--- front/src/App.js
+++ front/src/App.js
@@ -2,7 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import DiaryCreate from './pages/DiaryCreate/DiaryCreate';
 import PaperCreate from './pages/PaperCreate/PaperCreate';
 import PaperFeed from './pages/PaperFeed/PaperFeed';
-
+import BambooPage from './pages/BambooPage/BambooPage';
 function App() {
   return (
     <Routes>
@@ -13,6 +13,7 @@ function App() {
       <Route path="/paper">
         <Route path="/paper/create" element={<PaperCreate />} />
       </Route>
+      <Route path="/bamboo" element={<BambooPage />}></Route>
     </Routes>
   );
 }
