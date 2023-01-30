91dcb20cc03ef0f3b92281ddd742edaeeb2bd9eb
96hsjeong <96.hs.jeong@gmail.com>
2023-01-30 17:40:03 +0900

    [S08P12A809-207] Update: 컨디션 기능 구현

---
+++ back/imfine/src/main/java/com/idle/imfine/data/dto/user/response/ConditionResponseDto.java
@@ -0,0 +1,12 @@
+package com.idle.imfine.data.dto.user.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ConditionResponseDto {
+
+    private int condition;
+
+}
