e667a231756da886e3966d12f75348f2bb8ce721
김경림 <myway01212@naver.com>
2023-01-30 14:32:01 +0900

    [S08P12A809-210] Update: 질병/수술 선택 컴포넌트 생성

---
+++ front/src/components/PickSymptom/PickSymptom.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BoxRT25 } from '../common/BoxRT25/BoxRT25';
+
+function PickSymptom() {
+  return (
+    <div>
+      <h1>계정 기본 설정</h1>
+      <h3>질병/수술 |</h3>
+      <BoxRT25>질병/수술 선택</BoxRT25>
+    </div>
+  );
+}
+
+export default PickSymptom;
