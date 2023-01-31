1aebb3dc372661fb22a2a1b0bb75a3964704a9a0
hani <hanikim2057@gmail.com>
2023-01-31 15:07:39 +0900

    [S08P12A809-165] Update: Tab hooks 생성

--- front/src/components/ProfileContent/ProfileContent.js
+++ front/src/components/ProfileContent/ProfileContent.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import StatusCalendar from '../StatusCalendar/StatusCalendar';
+import { ProfileContentContainer, ProfileContentTabBtn } from './style';
 
 const ProfileContent = () => {
+  const useTabs = (initialTabIdx, allTabList) => {
+    const [contentIdx, setContentIdx] = useState(initialTabIdx);
+    return {
+      currentTab: allTabList[contentIdx],
+      setCurrentTab: setContentIdx,
+    };
+  };
+
+  const tabArr = [
+    { tabName: '달력', tabContent: <StatusCalendar /> },
+    { tabName: '일기장', tabContent: <span>일기장</span> },
+    { tabName: '구독중', tabContent: <span>구독중</span> },
+  ];
+  const { currentTab, setCurrentTab } = useTabs(0, tabArr);
+
+  console.log('cur', currentTab);
+
   return (
     <div>
-      ProfileContent
-      <StatusCalendar />;
+      <ProfileContentContainer>
+        <div style={{ display: 'flex' }}>
+          {tabArr.map((section, idx) => (
+            <ProfileContentTabBtn onClick={() => setCurrentTab(idx)}>
+              {section.tabName}
+            </ProfileContentTabBtn>
+          ))}
+        </div>
+        ProfileContent
+        {currentTab.tabContent}
+      </ProfileContentContainer>
     </div>
   );
 };
