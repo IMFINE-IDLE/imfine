85bc95f851694da3708e279a7c6257fb7746925b
96hsjeong <96.hs.jeong@gmail.com>
2023-01-31 13:03:19 +0900

    [S08P12A809-248] Update: CORS 에러 처리

--- back/imfine/src/main/java/com/idle/imfine/config/security/SecurityConfiguration.java
+++ back/imfine/src/main/java/com/idle/imfine/config/security/SecurityConfiguration.java
@@ -1,14 +1,19 @@
 package com.idle.imfine.config.security;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 
 // 어플리케이션의 보안 설정
@@ -23,30 +28,40 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.httpBasic().disable() // REST API는 UI를 사용하지 않으므로 기본설정을 비활성화
+        httpSecurity
+                .httpBasic().disable() // REST API는 UI를 사용하지 않으므로 기본설정을 비활성화
 
-            .csrf().disable() // REST API는 csrf 보안이 필요 없으므로 비활성화
+                .cors(cors -> cors.disable()) // CORS
+                .csrf().disable() // REST API는 csrf 보안이 필요 없으므로 비활성화
 
-            .sessionManagement()
-            .sessionCreationPolicy(
-                SessionCreationPolicy.STATELESS) // JWT Token 인증방식으로 세션은 필요 없으므로 비활성화
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // JWT Token 인증방식으로 세션은 필요 없으므로 비활성화
 
-            .and()
-            .authorizeRequests() // 리퀘스트에 대한 사용권한 체크
-            .antMatchers("/user/sign-up", "/user/sign-in", "/user/refresh", "/user/check/**").permitAll() // 가입 및 로그인 주소는 허용
-            .antMatchers("**exception**").permitAll()
+                .and()
+                .authorizeRequests() // 리퀘스트에 대한 사용권한 체크
+                .antMatchers("/user/sign-up", "/user/sign-in", "/user/refresh", "/user/check/**").permitAll() // 가입 및 로그인 주소는 허용
+                .antMatchers("**exception**").permitAll()
+                .anyRequest().hasAnyRole("USER", "ADMIN") // 나머지 요청은 인증된 USER, ADMIN만 접근 가능
 
-            .anyRequest().hasAnyRole("USER", "ADMIN") // 나머지 요청은 인증된 USER, ADMIN만 접근 가능
+                .and()
+                .exceptionHandling().authenticationEntryPoint(customAuthenticationEntryPoint)
+                .and()
+                .exceptionHandling().accessDeniedHandler(customAccessDeniedHandler)
 
-            .and()
-            .exceptionHandling().authenticationEntryPoint(customAuthenticationEntryPoint)
-            .and()
-            .exceptionHandling().accessDeniedHandler(customAccessDeniedHandler)
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
+                        UsernamePasswordAuthenticationFilter.class); // JWT Token 필터를 id/password 인증 필터 이전에 추가
 
-            .and()
-            .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
-                    UsernamePasswordAuthenticationFilter.class); // JWT Token 필터를 id/password 인증 필터 이전에 추가
+    }
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
