6d648eee17cecef33c7f717c6121a952f4274173
96hsjeong <96.hs.jeong@gmail.com>
2023-02-01 14:06:50 +0900

    [S08P12A809-205] Fix: 팔로우 예외처리

--- back/imfine/src/main/java/com/idle/imfine/errors/code/FollowErrorCode.java
+++ back/imfine/src/main/java/com/idle/imfine/errors/code/FollowErrorCode.java
@@ -12,9 +12,9 @@ public enum FollowErrorCode implements ErrorCode {
     CANNOT_FOLLOW_ME(HttpStatus.BAD_REQUEST, "나를 팔로우할 수 없습니다."),
     ALREADY_FOLLOWING(HttpStatus.BAD_REQUEST, "이미 팔로우 상태입니다."),
     NOT_ALREADY_FOLLOWING(HttpStatus.BAD_REQUEST, "이미 팔로우 중이 아닙니다."),
-    FOLLOWED_PRIVATE_USER(HttpStatus.BAD_REQUEST, "비공개 사용자를 팔로우했습니다.")
+    FOLLOWED_PRIVATE_USER(HttpStatus.BAD_REQUEST, "비공개 사용자를 팔로우했습니다."),
     ;
-ㄴ
+
     private final HttpStatus httpStatus;
     private final String message;
 
