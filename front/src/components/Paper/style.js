0f4cc25fc66a7f3f413e0edabdf4937d8b114323
skylar121 <myway01212@naver.com>
2023-02-02 01:07:03 +0900

    [S08P12A809-217] Update: 개별 일기 스타일 중간 커밋

--- front/src/components/Paper/style.js
+++ front/src/components/Paper/style.js
@@ -9,4 +9,17 @@ const BoxPaper = styled(BoxLT50)`
   z-index: 1;
 `;
 
-export { BoxPaper };
+const BoxLikeCmt = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const SpanLikeCmt = styled.label`
+  font-weight: 400;
+  color: var(--icon-color);
+  margin: 0 0.4em;
+`;
+
+export { BoxPaper, BoxLikeCmt, SpanLikeCmt };
