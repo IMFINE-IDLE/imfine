b0f6325a66262936df94a96fd1fe92654e034c36
2yoonjinny <2yoonjinny@naver.com>
2023-02-01 15:59:35 +0900

    [S08P12A809-198] Update: Textarea props 수정 및 화면 배치

--- front/src/pages/BambooCreatePage/BambooCreatePage.js
+++ front/src/pages/BambooCreatePage/BambooCreatePage.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import BambooHeader from '../../components/Bamboo/BambooHeader/BambooHeader';
 import NavBarBasic from '../../components/NavBarBasic/NavBarBasic';
+import TextareaGray from '../../components/common/TextareaGray/TextareaGray';
+import { DivTextArea } from './style';
+
 function BambooCreatePage() {
+  const [value, setValue] = useState('');
+
   return (
     <>
-      <NavBarBasic />
+      <NavBarBasic Back={true} Text={''} />
       <BambooHeader
         title={'대나무숲 외치기'}
         subTitle={'비밀대나무는'}
         secondSubTitle={'24시간 뒤에 사라져요.'}
+        bottom={'1em'}
       />
+      <DivTextArea>
+        <TextareaGray
+          width={'20em'}
+          margin={'1em'}
+          value={value}
+          setValue={setValue}
+        />
+      </DivTextArea>
     </>
   );
 }
---
+++ front/src/pages/BambooCreatePage/style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const DivTextArea = styled.div`
+  text-align: center;
+`;
+
+export { DivTextArea };
