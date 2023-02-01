90791fcd8ac61632a30612dcc19c597e66d53564
2yoonjinny <2yoonjinny@naver.com>
2023-02-01 14:26:13 +0900

    [S08P12A809-198] Update: 대나무작성 floating 버튼 컴포넌트 생성

---
+++ front/src/components/Bamboo/BambooBtnFloat/BambooBtnFloat.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { BoxBtnFloat, BtnLink, CircleFloat } from './style';
+import { FiEdit3 } from 'react-icons/fi';
+
+function BambooBtnFloat() {
+  const [isClicked, setIsClicked] = useState(false);
+
+  return (
+    <>
+      <BoxBtnFloat>
+        {isClicked && (
+          <>
+            <BtnLink
+              to="/bamboo/create"
+              style={{ width: '100%' }}
+              color={'light'}
+              margin={'0'}
+              padding={'1em 0.8em'}
+              radius={'50px'}
+              fontSize={'1em'}
+            >
+              대나무숲 외치러 가기
+            </BtnLink>
+          </>
+        )}
+        <CircleFloat onClick={() => setIsClicked((prev) => !prev)}>
+          <FiEdit3 />
+        </CircleFloat>
+      </BoxBtnFloat>
+    </>
+  );
+}
+
+export default BambooBtnFloat;
