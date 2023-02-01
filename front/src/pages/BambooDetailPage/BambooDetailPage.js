e959d4cb5702312274a7f3eed83b8c192408d2e4
yoonjinlee <yoonjinifer@gmail.com>
2023-02-02 02:34:52 +0900

    [S08P12A809-200] Update: 댓글컴포넌트 생성중

--- front/src/pages/BambooDetailPage/BambooDetailPage.js
+++ front/src/pages/BambooDetailPage/BambooDetailPage.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
 import BambooDetailHeader from '../../components/Bamboo/BambooDetailHeader/BambooDetailHeader';
 import NavBarBasic from '../../components/NavBarBasic/NavBarBasic';
-
-import { useParams } from 'react-router-dom';
+import BoxLeavesFeed from '../../components/Bamboo/BoxLeavesFeed/BoxLeavesFeed';
+// 댓글리스트 보여줄 컴포넌트 import 해오기
+// 해뜨면 까먹을까봐 일단 주석으로 정리
+// 강의실에서 작업할때 주석 지우기...
 
 function BambooDetailPage() {
   const params = useParams();
