9494e9a73843b01b432a7c540d8c7f9070494859
skylar121 <myway01212@naver.com>
2023-02-02 19:30:49 +0900

    [S08P12A809-266] Update: 일기 상세 api 가져오기

--- front/src/api/api.js
+++ front/src/api/api.js
@@ -14,6 +14,7 @@ const api = {
   },
   paper: {
     paperFeed: () => URL + PAPER + '/list',
+    paperDetail: (paperId) => URL + PAPER + paperId,
   },
 };
 
--- front/src/pages/PaperDetailPage/PaperDetailPage.js
+++ front/src/pages/PaperDetailPage/PaperDetailPage.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import api from '../../api/api';
 
 function PaperDetailPage() {
   const { paperId } = useParams();
+  // const [paper, setPaper] = useState(null);
+  // useEffect(() => {
+  //   const fetchPaperDetail = async () => {
+  //     const res = await axios.get(api.paper.paperDetail(paperId));
+  //     setPaper(res.data.data);
+  //   };
+  //   fetchPaperDetail();
+  // }, []);
+
   return (
     <div>
       PaperDetail
