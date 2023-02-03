8d754e3fbd57e9d2fce11a55a8b9c49758c5dfc6
hani <hanikim2057@gmail.com>
2023-02-03 13:54:45 +0900

    Update: 글로벌스타일 수정(폰트패밀리 적용)

--- front/src/styles/globalStyle.js
+++ front/src/styles/globalStyle.js
@@ -77,6 +77,15 @@ const GlobalStyle = createGlobalStyle`
   abbr {
     text-decoration: none;
   }
+
+  a,
+  div,
+  span,
+  input,
+  button,
+  textarea {
+    font-family: inherit;
+  }
 `;
 
 export { iconStyle, GlobalStyle };
