9baf0ed3d0ca805b54ef987ae40b4e4173d9aa78
jehyuck <wpgur07@gmail.com>
2023-02-04 22:57:46 +0900

    [S08P12A809-197] Update: 일기 에러처리

--- back/imfine/src/main/java/com/idle/imfine/errors/code/PaperErrorCode.java
+++ back/imfine/src/main/java/com/idle/imfine/errors/code/PaperErrorCode.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum PaperErrorCode implements ErrorCode {
 
     PAPER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 일기입니다."),
+    PAPER_DUPLICATE_DATE(HttpStatus.CONFLICT, "해당 날짜는 이미 작성했습니다.")
     ;
     private final HttpStatus httpStatus;
     private final String message;
