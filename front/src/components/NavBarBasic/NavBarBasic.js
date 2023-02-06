64ca867652d29ca0446f7ce8f464ca42aae314ab
2yoonjinny <2yoonjinny@naver.com>
2023-02-06 14:21:25 +0900

    Fix: 네비게이션바 주석 작성

--- front/src/components/NavBarBasic/NavBarBasic.js
+++ front/src/components/NavBarBasic/NavBarBasic.js
@@ -4,14 +4,16 @@ import { useNavigate, NavLink } from 'react-router-dom';
 import { BiChevronLeft, BiSearch, BiBell } from 'react-icons/bi';
 import NavBarRightItem from './NavBarRightItem';
 function NavBarBasic({ BackgroundColor, TextColor, Back, Text }) {
+  // 작성예시 :
+  // <NavBarBasic Back={true} Text={'일기장 작성하기'} BackgroundColor={'icon'} TextColor={'gray'} />
+
   // ** [0209] ** 기존 props의 path값은 삭제되었습니다.
 
   // Back: boolean parameter
   // Back == false면 로고있는 네비게이션 바
   // Back == true면 뒤로가기버튼있는 네비게이션 바
-  // <NavBarBasic Back={true} Text={'네비게이션 바 타이틀 값'} />
-
-  // BackgroundColor : 네비게이션 바 배경색상
+  // Text = 네비게이션 바 타이틀 값
+  // BackgroundColor : 네비게이션 바 배경색상 (default: var(--gray-color))
   // TextColor : 타이틀이 있는 네비게이션의 경우, props로 컬러변경 (default: var(--icon-color))
 
   const navigate = useNavigate();
