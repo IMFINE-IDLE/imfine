36b170802eb8c7089dc1d6a64168a834776440dc
wonyoung <isabella23@naver.com>
2023-02-07 12:48:17 +0900

    [S08P12A809-285] Update: 실시간 알림 구현중

---
+++ back/imfine/src/main/java/com/idle/imfine/data/dto/notification/response/ResponseNotification.java
@@ -0,0 +1,22 @@
+package com.idle.imfine.data.dto.notification.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ResponseNotification {
+    private long notificationId;
+    private int contentsCodeId;
+    //팔로우 경우 팔로우 아이디가 contentsId
+    private long contentsId;
+    private String senderUid;
+    private boolean isCheck;
+    private boolean showButton;
+}
--- back/imfine/src/main/java/com/idle/imfine/data/repository/notification/NotificationRepository.java
+++ back/imfine/src/main/java/com/idle/imfine/data/repository/notification/NotificationRepository.java
@@ -1,8 +1,10 @@
 package com.idle.imfine.data.repository.notification;
 
 import com.idle.imfine.data.entity.notification.Notification;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
-
+    Page<Notification> findByRecieverId(Long receiverId, Pageable pageable);
 }
