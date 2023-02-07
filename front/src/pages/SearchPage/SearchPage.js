dbd52828f0f3bdcd597b61e3093f2e0e671fef32
김경림 <myway01212@naver.com>
2023-02-07 15:05:54 +0900

    [S08P12A809-336] Update: 최근 검색어 리덕스에 저장 로직 추가

--- front/src/pages/SearchPage/SearchPage.js
+++ front/src/pages/SearchPage/SearchPage.js
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import SearchDiary from '../../components/Search/SearchDiary/SearchDiary';
 import SearchNavBar from '../../components/Search/SearchNavBar/SearchNavBar';
 import SearchPaper from '../../components/Search/SearchPaper/SearchPaper';
 import SearchResult from '../../components/Search/SearchResult/SearchResult';
 import SearchUser from '../../components/Search/SearchUser/SearchUser';
 import Tabs from '../../components/Tabs/Tabs';
+import { addSearchHistory } from '../../store/slice/userInfoSlice';
 import { BigCircle } from '../PaperFeedPage/style';
 
 function SearchPage() {
+  const dispatch = useDispatch();
   const [isSearching, setIsSearching] = useState(false);
   const [query, setQuery] = useState(''); // 검색창에 검색하는 쿼리
   const [queryResult, setQueryResult] = useState(''); // {{queryResult}}에 대한 검색결과 (검색완료한 쿼리)
@@ -15,8 +18,9 @@ function SearchPage() {
   const handleSearch = async (query) => {
     try {
       // query 가지고 서치 api 요청
-      console.log('제출');
+
       setIsSearching(true);
+      dispatch(addSearchHistory(query)); // 최근 검색어 저장
       setQueryResult(query);
     } catch (err) {
       console.log(err);
@@ -28,6 +32,7 @@ function SearchPage() {
     { idx: 1, tabName: '일기장', tabContent: <SearchDiary /> },
     { idx: 2, tabName: '유저', tabContent: <SearchUser /> },
   ];
+
   return (
     <>
       <SearchNavBar
