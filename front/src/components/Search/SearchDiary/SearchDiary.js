c0dc5995dc5198749756150d672d294d3aef0cf9
김경림 <myway01212@naver.com>
2023-02-07 13:53:47 +0900

    [S08P12A809-337] Update: 검색 결과 탭별 컴포넌트 생성

---
+++ front/src/components/Search/SearchDiary/SearchDiary.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SearchDiary() {
+  return (
+    <div>
+      SearchDiary
+      <div>일기장 검색결과</div>
+    </div>
+  );
+}
+
+export default SearchDiary;
--- front/src/pages/SearchPage/SearchPage.js
+++ front/src/pages/SearchPage/SearchPage.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
+import SearchDiary from '../../components/Search/SearchDiary/SearchDiary';
 import SearchNavBar from '../../components/Search/SearchNavBar/SearchNavBar';
 import SearchPaper from '../../components/Search/SearchPaper/SearchPaper';
 import SearchResult from '../../components/Search/SearchResult/SearchResult';
+import SearchUser from '../../components/Search/SearchUser/SearchUser';
 import TabBar from '../../components/TabBar/TabBar';
 import Tabs from '../../components/Tabs/Tabs';
 
@@ -19,8 +21,8 @@ function SearchPage() {
 
   const tabArr = [
     { idx: 0, tabName: '일기', tabContent: <SearchPaper /> },
-    { idx: 1, tabName: '일기장', tabContent: <SearchPaper /> },
-    { idx: 2, tabName: '유저', tabContent: <SearchPaper /> },
+    { idx: 1, tabName: '일기장', tabContent: <SearchDiary /> },
+    { idx: 2, tabName: '유저', tabContent: <SearchUser /> },
   ];
   return (
     <>
