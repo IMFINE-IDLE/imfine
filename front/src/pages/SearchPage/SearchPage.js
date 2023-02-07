a580bd9773552b0e9ef11d0cb3b775e617e76d30
김경림 <myway01212@naver.com>
2023-02-07 14:28:59 +0900

    [S08P12A809-335] Update: 검색 여부에 따라 페이지 분리

--- front/src/pages/SearchPage/SearchPage.js
+++ front/src/pages/SearchPage/SearchPage.js
@@ -4,16 +4,18 @@ import SearchNavBar from '../../components/Search/SearchNavBar/SearchNavBar';
 import SearchPaper from '../../components/Search/SearchPaper/SearchPaper';
 import SearchResult from '../../components/Search/SearchResult/SearchResult';
 import SearchUser from '../../components/Search/SearchUser/SearchUser';
-import TabBar from '../../components/TabBar/TabBar';
 import Tabs from '../../components/Tabs/Tabs';
+import { BigCircle } from '../PaperFeedPage/style';
 
 function SearchPage() {
+  const [isSearching, setIsSearching] = useState(false);
   const [query, setQuery] = useState('');
 
   const handleSearch = async (query) => {
     try {
       // query 가지고 서치 api 요청
       console.log('제출');
+      setIsSearching(true);
     } catch (err) {
       console.log(err);
     }
@@ -31,11 +33,21 @@ function SearchPage() {
         setQuery={setQuery}
         handleSearch={handleSearch}
       />
-      <div>
-        <SearchResult query={query} />
-        <Tabs tabArr={tabArr} btnWidth={'6.2em'} />
-        {/* <TabBar /> */}
-      </div>
+      {isSearching ? (
+        <div>
+          <SearchResult query={query} />
+          <Tabs tabArr={tabArr} btnWidth={'6.2em'} />
+          {/* <TabBar /> */}
+        </div>
+      ) : (
+        <div>
+          <div>
+            <h2>최근 검색어</h2>
+          </div>
+          <div>블라블라</div>
+          <BigCircle />
+        </div>
+      )}
     </>
   );
 }
