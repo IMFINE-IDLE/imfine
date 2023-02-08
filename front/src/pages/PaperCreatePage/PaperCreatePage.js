9f80d8e30ca04a46162171c61670f17b7c7cd064
2yoonjinny <2yoonjinny@naver.com>
2023-02-08 12:43:07 +0900

    [S08P12A809-224] Update: 증상Score 등록 컴포넌트 만들기

--- front/src/pages/PaperCreatePage/PaperCreatePage.js
+++ front/src/pages/PaperCreatePage/PaperCreatePage.js
@@ -2,15 +2,27 @@ import axios from 'axios';
 import api from '../../api/api';
 import React, { useState, useEffect } from 'react';
 import NavBarBasic from '../../components/NavBarBasic/NavBarBasic';
-import { BoxPaperDetail, BoxContent } from './style';
+import {
+  BoxPaperDetail,
+  BoxContent,
+  TopDiv,
+  ContentLabel,
+  RightDiv,
+} from './style';
 import PaperCreateHeader from '../../components/Paper/PaperCreateHeader/PaperCreateHeader';
 import DiariesDropdown from '../../components/Paper/DiariesDropdown/DiariesDropdown';
 import DateDropdown from '../../components/Paper/DateDropdown/DateDropdown';
 import SymptomRating from '../../components/Paper/SymptomRating/SymptomRating';
+import { useNavigate } from 'react-router-dom';
+import { FiArrowRight } from 'react-icons/fi';
+
 function PaperCreatePage() {
+  const navigate = useNavigate();
+  // 이미지 업로드 개수 3개까지 MAX
   const now = new Date();
-  const [diaries, setDiaries] = useState([]);
-  const [diaryTitle, setDiaryTitle] = useState('');
+  const [diaries, setDiaries] = useState([]); // dropdown에 나올 일기장 선택값들 저장
+  const [diaryId, setDiaryId] = useState(''); // dropdown 선택 시 일기장 아이디값 저장
+  const [diary, setDiary] = useState(''); // 일기장 상세정보
   const [form, setForm] = useState({
     year: now.getFullYear(),
     month: '01',
@@ -20,23 +32,44 @@ function PaperCreatePage() {
   console.log('dddd', form.day);
   console.log('mmmm', form.month);
   console.log('yyyy', form.year);
+
+  // 사용자가 작성한 다이어리 정보 받아오기
   const getDiaries = async () => {
     try {
       const res = await axios.get(api.diary.getDiaries(), {
         headers: { Authorization: localStorage.getItem('accessToken') },
       });
       setDiaries(res.data.data);
-      console.log('다이어리테스트', res.data.data);
+      console.log('내가만든다이어리', res.data.data);
     } catch (res) {
       console.log('err', res.data);
     }
   };
   useEffect(() => {
     getDiaries();
-    console.log('diaries', diaries);
-  }, []);
+  }, [diaryId]);
 
-  console.log('diaryid', diaryTitle);
+  console.log('selected diaryid', diaryId);
+
+  // 해당 다이어리의 정보 불러오기
+  const getDiaryInfos = async () => {
+    try {
+      const res = await axios.get(api.diary.getDiaryInfo(diaryId), {
+        headers: { Authorization: localStorage.getItem('accessToken') },
+      });
+      console.log('일기장 상세정보', res.data.data);
+      setDiary(res.data.data);
+    } catch (res) {
+      console.log('err', res.data);
+    }
+  };
+
+  useEffect(() => {
+    if (diaryId != '') {
+      getDiaryInfos();
+    }
+  }, [diaryId]);
+
   return (
     <>
       <NavBarBasic
@@ -47,16 +80,25 @@ function PaperCreatePage() {
       />
       <BoxPaperDetail>
         <PaperCreateHeader />
-        <DiariesDropdown
-          value={diaryTitle}
-          state={setDiaryTitle}
-          diaries={diaries}
-        />
+        <DiariesDropdown value={diaryId} state={setDiaryId} diaries={diaries} />
         <DateDropdown value={form} state={setForm} />
       </BoxPaperDetail>
+      <TopDiv>
+        <ContentLabel>증상을 체크해주세요.</ContentLabel>
+      </TopDiv>
       <BoxContent>
         <SymptomRating />
       </BoxContent>
+      <RightDiv>
+        <FiArrowRight />
+        <ContentLabel
+          margin="1em 2em 1em 0.3em"
+          onClick={() => navigate('/paper/symptom')}
+        >
+          {' '}
+          증상 추가하러 가기
+        </ContentLabel>
+      </RightDiv>
     </>
   );
 }
--- front/src/pages/PaperCreatePage/style.js
+++ front/src/pages/PaperCreatePage/style.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { BoxNoShad } from '../../components/common/BoxNoShad/BoxNoShad';
+import { FlexDiv } from '../../components/common/FlexDiv/FlexDiv';
 const BoxPaperDetail = styled(BoxNoShad)`
   min-height: 100px;
   border-radius: 0px 0px 50px 50px;
@@ -8,9 +9,28 @@ const BoxPaperDetail = styled(BoxNoShad)`
 `;
 
 const BoxContent = styled.div`
-  padding: 1em 0;
-  line-height: 1.2em;
-  color: var(--icon-color);
+  display: flex;
+  justify-content: center;
+  align-item: center;
+  margin: 1em;
 `;
 
-export { BoxPaperDetail, BoxContent };
+const TopDiv = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const RightDiv = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`;
+
+const ContentLabel = styled.label`
+  font-weight: ${(props) => props.weight || '700'};
+  color: var(--icon-color);
+  margin: ${(props) => props.margin || '1.5em 0 1.5em 2em'};
+`;
+
+export { BoxPaperDetail, BoxContent, TopDiv, ContentLabel, RightDiv };
