67d48504f70fd874b64d57d42afbf9d4aee4d8b5
hani <hanikim2057@gmail.com>
2023-02-09 11:09:23 +0900

    [S08P12A809-164] Update: 프로필 일기장, 구독중 탭 기능 구현

--- front/src/components/Profile/ProfileSubscribeDiary/ProfileSubscribeDiary.js
+++ front/src/components/Profile/ProfileSubscribeDiary/ProfileSubscribeDiary.js
@@ -3,14 +3,13 @@ import api from '../../../api/api';
 import { axiosInstance } from '../../../api/axiosInstance';
 import DiaryListGrid from '../../Diary/DiaryListGrid/DiaryListGrid';
 
-const ProfileSubscribeDiary = (uid) => {
+const ProfileSubscribeDiary = ({ uid }) => {
   const [diaryList, setDiaryList] = useState(null);
 
   // 유저가 구독중인 일기장 목록 가져오기
   const fetchGetUserSubscribeDiaryList = async () => {
     try {
-      const res = await axiosInstance(api.diary.getUserDiaryList(uid));
-      console.log(res.data.data);
+      const res = await axiosInstance(api.diary.getUserSubscribeDiaryList(uid));
       await setDiaryList(res.data.data);
     } catch (err) {
       console.error(err);
--- front/src/components/Profile/ProfileUserDiary/ProfileUserDiary.js
+++ front/src/components/Profile/ProfileUserDiary/ProfileUserDiary.js
@@ -3,14 +3,13 @@ import DiaryListGrid from '../../Diary/DiaryListGrid/DiaryListGrid';
 import { axiosInstance } from '../../../api/axiosInstance';
 import api from '../../../api/api';
 
-const ProfileUserDiary = (uid) => {
+const ProfileUserDiary = ({ uid }) => {
   const [diaryList, setDiaryList] = useState(null);
 
   // 유저가 쓴 일기장 목록 가져오기
   const fetchGetUserDiaryList = async () => {
     try {
       const res = await axiosInstance(api.diary.getUserDiaryList(uid));
-      console.log(res.data.data);
       await setDiaryList(res.data.data);
     } catch (err) {
       console.error(err);
