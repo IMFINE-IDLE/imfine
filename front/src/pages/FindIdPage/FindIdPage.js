5db9aa7a6aadb7058e5c22395a07a78ac31c959f
김경림 <myway01212@naver.com>
2023-02-13 13:28:46 +0900

    중간 커밋

--- front/src/pages/FindIdPage/FindIdPage.js
+++ front/src/pages/FindIdPage/FindIdPage.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import NavBarBasic from '../../components/NavBarBasic/NavBarBasic';
+import { BoxGrad } from '../../components/common/BoxGrad/BoxGrad';
 
 function FindIdPage() {
   return (
-    <div>
-      FindIdPage
-      <div>아이디찾기</div>
-    </div>
+    <BoxGrad radius={'0 0 0 0'}>
+      <NavBarBasic Text={'아이디 찾기'} Back />
+    </BoxGrad>
   );
 }
 
