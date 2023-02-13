fd5b6a8f5008c223086b6954e6f5c98900565ca5
wonyoung <isabella23@naver.com>
2023-02-13 16:44:43 +0900

    [S08P12A809-401] Refactoring: 대나무

--- back/imfine/src/main/java/com/idle/imfine/service/notification/NotificationService.java
+++ back/imfine/src/main/java/com/idle/imfine/service/notification/NotificationService.java
@@ -3,6 +3,7 @@ package com.idle.imfine.service.notification;
 import com.idle.imfine.data.dto.notification.request.RequestNotificationDetailDto;
 import com.idle.imfine.data.dto.notification.response.ResponseNotification;
 import com.idle.imfine.data.dto.notification.response.ResponseNotificationPost;
+import com.idle.imfine.data.entity.notification.Notification;
 import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -13,6 +14,6 @@ public interface NotificationService {
     void checkNotification(RequestNotificationDetailDto requestNotificationDetailDto, String uid);
     SseEmitter subscribe(String uid, String lastEventId);
     void dtoToSend(ResponseNotificationPost responseDto);
-    void send(Long senderId, Long recieverId, int contenstsCodeId, Long contentsId, int type);
+    void send(Notification notification);
 
 }
