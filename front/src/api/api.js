759bd96559de39c2a4adf3dcd6b20ae45d100c97
김경림 <myway01212@naver.com>
2023-02-15 10:16:53 +0900

    Chore: api URL 주석 변경

--- front/src/api/api.js
+++ front/src/api/api.js
@@ -1,5 +1,5 @@
-const URL = 'http://localhost:8080/api';
-// const URL = 'https://i8a809.p.ssafy.io/api';
+// const URL = 'http://localhost:8080/api';
+const URL = 'https://i8a809.p.ssafy.io/api';
 
 const USERS = '/user';
 const CHECK = '/check';
