8151001dda4e92899b267cf74bd708419950fc2d
김경림 <myway01212@naver.com>
2023-02-15 11:01:27 +0900

    [S08P12A809-392] Update: utils 파일 분리 및 paper utils 추가

---
+++ front/src/utils/paperUtils.js
@@ -0,0 +1,34 @@
+/**
+ * 게시글 시간 표시 함수
+ */
+export function getTimeDifference(createdAt) {
+  let currentTime = new Date();
+  let providedTime = new Date(createdAt);
+  let milli = currentTime.getTime() - providedTime.getTime();
+  let timeGap = parseInt(milli / 60000);
+  // console.log(paperId, timeGap);
+
+  if (timeGap < 60) {
+    return `${timeGap}분전`;
+  } else if (timeGap >= 60 && timeGap < 60 * 24) {
+    return `${parseInt(timeGap / 60)}시간전`;
+  } else if (timeGap >= 60 * 24) {
+    if (currentTime.getFullYear() - providedTime.getFullYear()) {
+      return `${providedTime.getFullYear()}년 ${
+        providedTime.getMonth() + 1
+      }월 ${providedTime.getDate()}일`;
+    } else {
+      return `${providedTime.getMonth() + 1}월 ${providedTime.getDate()}일`;
+    }
+  }
+}
+
+/**
+ * 일기 날짜 표시 함수
+ */
+export function getPaperDate(date) {
+  let diaryTime = new Date(date);
+  return `${diaryTime.getFullYear()}년 ${
+    diaryTime.getMonth() + 1
+  }월 ${diaryTime.getDate()}일`;
+}
