c7ec1d00b284f1d6e41f078e02c48c1d5360b8ba
hani <hanikim2057@gmail.com>
2023-02-15 11:00:17 +0900

    [S08P12A809-404] Fix: 다른 사람 팔로워 목록에서는 삭제버튼 아니라 팔로우 버튼 보이도록 수정

--- front/src/components/Profile/FollowList/FollowList.js
+++ front/src/components/Profile/FollowList/FollowList.js
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 import FollowUser from '../FollowUser/FollowUser';
 import { FollowListContainer } from './style';
 
 // user정보 객체들을 담은 배열을 users로 넘겨줄 것
 // type은 'following' 또는 'follower' (디폴트는 'following')
 const FollowList = ({ users, type, setTrigger, noFollowButton }) => {
+  const { uid } = useParams();
+  const isMine = Boolean(uid === useSelector((state) => state.user.uid));
+
   return (
     <>
       <FollowListContainer>
@@ -18,6 +23,7 @@ const FollowList = ({ users, type, setTrigger, noFollowButton }) => {
             setTrigger={setTrigger}
             key={user.uid}
             noFollowButton={noFollowButton}
+            isMine={isMine}
           />
         ))}
       </FollowListContainer>
