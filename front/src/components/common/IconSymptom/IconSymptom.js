a5e8758c05280fddfc9451d4d978c0ba71de783e
hani <hanikim2057@gmail.com>
2023-02-17 10:48:38 +0900

    [S08P12A809-406] Update: 메뉴 체크시 체크표시 추가

--- front/src/components/common/IconSymptom/IconSymptom.js
+++ front/src/components/common/IconSymptom/IconSymptom.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { TiInputChecked } from 'react-icons/ti';
 import { FlexDiv } from '../FlexDiv/FlexDiv';
 import { BoxImg, TextImg } from './style';
 
-function IconSymptom({ type, id, name, image, handleMenuClick }) {
+function IconSymptom({ type, id, name, image, handleMenuClick, clicked }) {
   return (
     <FlexDiv
       onClick={handleMenuClick}
@@ -10,9 +11,25 @@ function IconSymptom({ type, id, name, image, handleMenuClick }) {
       direction="column"
       justify="start"
     >
-      <BoxImg color={'gray'}>
-        <img src={image} style={{ width: '100%', height: '100%' }} alt={name} />
-      </BoxImg>
+      <FlexDiv width="auto" height="auto" style={{ position: 'relative' }}>
+        <BoxImg color={'gray'}>
+          <img
+            src={image}
+            style={{ width: '100%', height: '100%' }}
+            alt={name}
+          />
+        </BoxImg>
+        {clicked && (
+          <TiInputChecked
+            style={{
+              position: 'absolute',
+              width: '1.8em',
+              height: '1.8em',
+              fill: '#A9D7D0',
+            }}
+          />
+        )}
+      </FlexDiv>
       <TextImg>{name}</TextImg>
     </FlexDiv>
   );
